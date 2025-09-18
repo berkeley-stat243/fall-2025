ba363ca822aae6133dba7072c411fb1cb049837b
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-09-18 11:31:05 -0700

    Make some minor-ish edits to Unit 5.

--- units/mypkg/mysubpkg/values.py
+++ units/mypkg/mysubpkg/values.py
@@ -1,3 +1,7 @@
-_x = 999
+x = 999
 b = 7
-d = 9
+
+# Use of `_` is a convention for internal/private objects.
+# And `from M import *` would not import objects whose
+# names start with an underscore.
+_my_internal_var = 9   
